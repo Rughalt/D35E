0ee9d42626ab8a65559daced439e3246ef6267c4
Antoni Sobkowicz <rughalt@gmail.com>
2021-05-06 06:27:45 +0200

    Hotfix

--- module/actor/entity.js
+++ module/actor/entity.js
@@ -5992,7 +5992,7 @@ export class ActorPF extends Actor {
             return;
 
         function isActionRollable(_action) {
-            if (_action.indexOf("://")) return false;
+            if (_action.indexOf("://") !== -1) return false;
             return /^(.*?[0-9]d[0-9]+.*?)$/.test(_action)
                 || _action.indexOf("max") !== -1
                 || _action.indexOf("min") !== -1
